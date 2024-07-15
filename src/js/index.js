2fdc708959d38ce612b97a60d90c242d30da7986
Wendel Santos <67715702+dellstark@users.noreply.github.com>
2024-07-15 11:28:22 -0300

    Script Contador

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
-// como fazer operações matematicas simples
+/* 
 
-// const PRECO = 2;
-// var desconto = 0.2;
-// total = PRECO - desconto;
-// console.log(total)
+var currentNumber = document.getElementById('currentNumber');
+
+function increment(){
+    currentNumber.innerHTML = parseInt(currentNumber.innerHTML) + 1;
+}
+function decrement(){
+    currentNumber.innerHTML = parseInt(currentNumber.innerHTML) - 1;
+}
+
+function contador(){
+    var currentNumber = document.getElementById('currentNumber');
+    return currentNumber.ariaColCount.length();
+}
+    
+*/
+
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
